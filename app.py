2fa5f65361b1422770e6475871af19d18803635b
Nicole Murison <nicole@Nicoles-MacBook-Air.local>
2025-03-31 23:03:59 -0400

    Updated Streamlit app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import os
 
-st.title("My Jupyter Notebook")
-st.markdown(open("Assignment7.ipynb").read())
+PORT = int(os.environ.get("PORT", 10000)) 
+
+st.title("My Deployed Streamlit App")
+st.write("Hello, this is my web app!")
+
+if __name__ == "__main__":
+    st.run(port=PORT, address="0.0.0.0")
